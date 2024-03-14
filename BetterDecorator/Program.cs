4b1176366f83e0ffb995a3c8209a6ea17d2e2c9b
Daniel Costa <danielrcosta.dev@gmail.com>
2024-03-14 18:20:40 +0000

    Program accepts 3 arguments

---
+++ BetterDecorator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BetterDecorator
+{
+    class Program
+    {
+        private static string Decor(string s, char dec, int num)
+        {
+            string destr = ""; 
+            for (int i = 0; i < num; i++)
+            {
+                destr += $"{dec}";
+            }
+            destr += $"{s}";
+
+            for (int i = 0; i < num; i++)
+            {
+                destr += $"{dec}";
+            }
+            return destr;
+        }
+        
+        static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.WriteLine
+                return;
+            }
+
+            string text = args[0];
+            char decorationChar = args[1][0];
+            int times = int.Parse(args[2]);
+
+            Console.WriteLine(Decor(text, decorationChar, times));
+        }
+    }
+}
