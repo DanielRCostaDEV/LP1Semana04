cd2a0331a4667bdb60c1c3789187fd78b1808e9a
Daniel Costa <danielrcosta.dev@gmail.com>
2024-03-14 18:45:13 +0000

    Asks user for the value of n

---
+++ Special/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Special
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insert n-th number of the special sequence: ");
+            int n = int.Parse(Console.ReadLine());
+        }
+
+    }
+}
